0a8efb56e76907b416ed8a015126476988616678
Jen Looper <jen.looper@gmail.com>
2020-04-11 21:00:53 -0400

    removing script embed - breaks build

--- game/.vuepress/config.js
+++ game/.vuepress/config.js
@@ -4,22 +4,6 @@ module.exports = {
 	postcss: {
 		plugins: [require('autoprefixer'), require('tailwindcss')('./tailwind.config.js')]
 	},
-	head: [
-		[
-			'script',
-			{},
-			`
-        var _hmt = _hmt || []; 
-        (function() { 
-		var hm = document.createElement("script"); 
-		hm.type = "text/javascript",
-        hm.src = "https://download.playfab.com/PlayFabClientApi.js"; 
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s); 
-        })(); 
-    `
-		]
-	],
 	themeConfig: {
 		search: false,
 		lastUpdated: 'Last Updated'
