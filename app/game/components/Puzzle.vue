4ec57d437132085a07d69449a0a4b13de2318f65
Jen Looper <jen.looper@gmail.com>
2020-05-12 12:59:42 -0400

    badge, twitter first pass

--- app/game/components/Puzzle.vue
+++ app/game/components/Puzzle.vue
@@ -51,10 +51,21 @@
         </label>
       </div>
     </div>
-    <p
-      class="font-bold"
-      v-if="show"
-    >The stones click into place, and you hear a door creak open beyond the stairs leading down. Suddenly, the floor tilts under you and you start to slide towards the staircase! What will happen? Come back next month to continue the Azure Maya Mystery!</p>
+    <div v-if="show">
+      <p
+        class="font-bold"
+      >The stones click into place, and you hear a door creak open beyond the stairs leading down. Suddenly, the floor tilts under you and you start to slide towards the staircase! What will happen? Come back next month to continue the Azure Maya Mystery!</p>
+      <p>Congratulations on completing the first of three parts of the Azure Maya Mystery!</p>
+      <img src="/images/badge1.png" alt="badge" />
+
+      <a
+        href="https://twitter.com/intent/tweet?url=https%3A%2F%2Flively-sea-026c3791e.azurestaticapps.net%2F&text=I%20just%20entered%20the%20Azure%20Maya%20Mystery%20pyramid.%20Look%20forward%20to%20next%20month%20when%20I%20will%20get%20to%20explore%20more.&hashtags=AzureMayaMystery"
+        class="twitter-hashtag-button"
+        data-show-count="false"
+      >Tell the world!</a>
+
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
   </div>
 </template>
 <script>
