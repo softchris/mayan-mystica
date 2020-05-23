e65083586fb6609f8614188249c8c797089ce9d4
Jen Looper <jen.looper@gmail.com>
2020-05-23 12:29:57 -0400

    removing routes.json and removing call to items in Page.vue

--- app/game/.vuepress/components/Page.vue
+++ app/game/.vuepress/components/Page.vue
@@ -1,65 +1,71 @@
 <template>
-	<div class="prompt">
-		<span class="font-bold" v-if="show">{{ instructions }}</span>
-		<div class="page-nav">
-			<router-link :to="link || './'">{{ action }}</router-link>
-		</div>
-	</div>
+  <div class="prompt">
+    <span class="font-bold" v-if="show">{{ instructions }}</span>
+    <div class="page-nav">
+      <router-link :to="link || './'">{{ action }}</router-link>
+    </div>
+  </div>
 </template>
 <script>
-const items = require('@theme/utils/items.json');
-import { hasItem, getUID, setUID, hasUID, setSessionTicket } from '@theme/utils/helpers';
-import axios from 'axios';
+//const items = require('@theme/utils/items.json');
+import {
+  hasItem,
+  getUID,
+  setUID,
+  hasUID,
+  setSessionTicket
+} from "@theme/utils/helpers";
+import axios from "axios";
 export default {
-	created() {
-		//this.getInventory();
-		if (!hasUID()) {
-			setUID();
-		}
-		axios
-			.post('/api/loginAnon', {
-				id: getUID(),
-				createAccount: hasUID(),
-			})
-			.then((response) => {
-				console.log(response);
-				if (response.data.errorMessage == null) {
-					setSessionTicket(response.data.SessionTicket);
-				}
-			})
-			.catch(function(error) {
-				console.log(error);
-			});
-	},
-	data() {
-		this.$root.$on('item_added', (id) => {
-			this.getInventory();
-		});
+  created() {
+    //this.getInventory();
+    if (!hasUID()) {
+      setUID();
+    }
+    axios
+      .post("/api/loginAnon", {
+        id: getUID(),
+        createAccount: hasUID()
+      })
+      .then(response => {
+        console.log(response);
+        if (response.data.errorMessage == null) {
+          setSessionTicket(response.data.SessionTicket);
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  },
+  data() {
+    this.$root.$on("item_added", id => {
+      this.getInventory();
+    });
 
-		this.$root.$on('showResult', (id) => {
-			this.getInventory();
-		});
+    this.$root.$on("showResult", id => {
+      this.getInventory();
+    });
 
-		return {
-			show: false,
-			link: this.url,
-		};
-	},
-	methods: {
-		getInventory() {
-			console.log(this.condition);
-			console.log(hasItem(this.condition));
-			if (this.initialHide) {
-				this.show = false;
-			} else {
-				this.show = true;
-			}
-		},
-		goToRoom(url) {
-			this.$router.replace({ path: url });
-		},
-	},
-	props: ['url', 'action', 'condition', 'instructions'],
+    return {
+      show: false,
+      link: this.url
+    };
+  },
+  methods: {
+    getInventory() {
+      console.log(this.condition);
+      console.log(hasItem(this.condition));
+      if (this.initialHide) {
+        this.show = false;
+      } else {
+        this.show = true;
+      }
+    },
+    goToRoom(url) {
+      this.$router.replace({ path: url });
+    }
+  },
+  props: ["url", "action", "condition", "instructions"]
 };
 </script>
 <style lang="stylus">
