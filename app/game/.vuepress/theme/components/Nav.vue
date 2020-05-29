6289462923f3c240430cb4f564e0aa7bed2aabd2
Jen Looper <jen.looper@gmail.com>
2020-05-29 18:31:31 -0400

    accessibility issue - extra h1s

--- app/game/.vuepress/theme/components/Nav.vue
+++ app/game/.vuepress/theme/components/Nav.vue
@@ -1,29 +1,29 @@
 <template>
-	<nav
-		class="nav rounded-t flex items-center justify-between flex-wrap text-right pr-0 lg:pr-5 md:pr-3 sm:pr-0 border-b"
-	>
-		<img :src="$withBase('/images/logo.png')" alt="Microsoft logo" />
+  <nav
+    class="nav rounded-t flex items-center justify-between flex-wrap text-right pr-0 lg:pr-5 md:pr-3 sm:pr-0 border-b"
+  >
+    <img :src="$withBase('/images/logo.png')" alt="Microsoft logo" />
 
-		<router-link class="cursor-pointer block m-1 lg:m-4 md:m-2 sm:m-1" :to="'/'">
-			<h1 class="text-white">Azure Maya Mystery</h1>
-		</router-link>
-	</nav>
+    <router-link class="cursor-pointer block m-1 lg:m-4 md:m-2 sm:m-1" :to="'/'">
+      <span class="title">Azure Maya Mystery</span>
+    </router-link>
+  </nav>
 </template>
 
 <script>
 export default {
-	name: 'Nav',
-	data() {
-		return {
-			hidden: 'lg:inline-block md:hidden sm:hidden hidden',
-		};
-	},
-	methods: {},
+  name: "Nav",
+  data() {
+    return {
+      hidden: "lg:inline-block md:hidden sm:hidden hidden"
+    };
+  },
+  methods: {}
 };
 </script>
 
 <style scoped>
 .markdown-body a {
-	text-decoration: none;
+  text-decoration: none;
 }
 </style>
--- app/game/.vuepress/theme/layouts/BasicLayout.vue
+++ app/game/.vuepress/theme/layouts/BasicLayout.vue
@@ -1,40 +1,40 @@
 <template>
-	<div class="markdown-body">
-		<router-link to="/pyramid/es" class="float-right p-5 white-link">
-			<span @click="setLanguage('es')">Español</span>
-		</router-link>
-		<router-link to="/pyramid/pt" class="float-right p-5 white-link">
-			<span @click="setLanguage('pt')">Português</span>
-		</router-link>
-		<router-link to="/" class="float-right p-5 white-link">
-			<span @click="setLanguage('en')">English</span>
-		</router-link>
+  <div class="markdown-body">
+    <router-link to="/pyramid/es" class="float-right p-5 white-link">
+      <span @click="setLanguage('es')">Español</span>
+    </router-link>
+    <router-link to="/pyramid/pt" class="float-right p-5 white-link">
+      <span @click="setLanguage('pt')">Português</span>
+    </router-link>
+    <router-link to="/" class="float-right p-5 white-link">
+      <span @click="setLanguage('en')">English</span>
+    </router-link>
 
-		<router-link to="/pyramid/login" class="float-right p-5 white-link">My Adventure</router-link>
-		<h1 class="text-3xl pb-5 pt-5 ml-5 text-sans">{{ $page.frontmatter.title }}</h1>
-		<div class="p-5 bg-white m-5 rounded">
-			<Content />
-		</div>
-	</div>
+    <router-link to="/pyramid/login" class="float-right p-5 white-link">My Adventure</router-link>
+    <p class="text-3xl pb-5 pt-5 ml-5 text-sans">{{ $page.frontmatter.title }}</p>
+    <div class="p-5 bg-white m-5 rounded">
+      <Content />
+    </div>
+  </div>
 </template>
 
 <script>
-import { setLocale, getLocale } from '@theme/utils/helpers';
+import { setLocale, getLocale } from "@theme/utils/helpers";
 
 export default {
-	name: 'BasicLayout',
-	i18n: {},
-	methods: {
-		setLanguage(lang) {
-			setLocale(lang);
-			this.$root.$emit('lang_changed', lang);
-		},
-	},
+  name: "BasicLayout",
+  i18n: {},
+  methods: {
+    setLanguage(lang) {
+      setLocale(lang);
+      this.$root.$emit("lang_changed", lang);
+    }
+  }
 };
 </script>
 
 <style scoped>
 .white-link {
-	color: white;
+  color: white;
 }
 </style>
