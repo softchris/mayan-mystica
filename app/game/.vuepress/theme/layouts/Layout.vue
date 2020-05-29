35ac0c0f5f1dabd58ca9d9b0dc756793408a242b
Jen Looper <jen.looper@gmail.com>
2020-05-29 15:57:38 -0400

    proper path for backdrops

--- app/game/.vuepress/theme/layouts/Layout.vue
+++ app/game/.vuepress/theme/layouts/Layout.vue
@@ -1,51 +1,57 @@
 <template>
-	<div class="wrapper">
-		<div class="markdown-body font-serif bg-white m-2 sm:m-3 md:m-6 lg:m-12 text-lg rounded-lg">
-			<Nav />
-			<div role="banner" v-if="hasBackdrop" :style="backdrop" class="w-full image">&nbsp;</div>
-			<div class="flex mb-4">
-				<div role="complementary" class="w-1/2">
-					<component :is="camera"></component>
-				</div>
-				<div role="main" class="w-3/4">
-					<component :is="layout"></component>
-				</div>
-			</div>
+  <div class="wrapper">
+    <div class="markdown-body font-serif bg-white m-2 sm:m-3 md:m-6 lg:m-12 text-lg rounded-lg">
+      <Nav />
+      <div
+        role="banner"
+        v-if="hasBackdrop"
+        :style="{ backgroundImage: 'url(' + hasBackdrop + ')' }"
+        class="w-full image backdrop"
+      >&nbsp;</div>
+      <div class="flex mb-4">
+        <div role="complementary" class="w-1/2">
+          <component :is="camera"></component>
+        </div>
+        <div role="main" class="w-3/4">
+          <component :is="layout"></component>
+        </div>
+      </div>
 
-			<Footer />
-		</div>
-	</div>
+      <Footer />
+    </div>
+  </div>
 </template>
 <script>
-import Nav from '@theme/components/Nav.vue';
-import Footer from '@theme/components/Footer.vue';
-import BasicLayout from '@theme/layouts/BasicLayout.vue';
-import Camera from '@theme/components/Camera.vue';
+import Nav from "@theme/components/Nav.vue";
+import Footer from "@theme/components/Footer.vue";
+import BasicLayout from "@theme/layouts/BasicLayout.vue";
+import Camera from "@theme/components/Camera.vue";
 
 export default {
-	components: {
-		Nav,
-		Footer,
-		BasicLayout,
-		Camera,
-	},
-	computed: {
-		layout() {
-			return this.$page.frontmatter.layout || 'BasicLayout';
-		},
-		camera() {
-			return this.$page.frontmatter.camera || 'Camera';
-		},
-		hasBackdrop() {
-			return this.$page.frontmatter.backdrop || false;
-		},
-		backdrop() {
-			return {
-				'background-image': `url(${this.$page.frontmatter.backdrop})`,
-				'background-repeat': 'no-repeat',
-				'background-size': 'cover',
-			};
-		},
-	},
+  components: {
+    Nav,
+    Footer,
+    BasicLayout,
+    Camera
+  },
+  computed: {
+    layout() {
+      return this.$page.frontmatter.layout || "BasicLayout";
+    },
+    camera() {
+      return this.$page.frontmatter.camera || "Camera";
+    },
+    hasBackdrop() {
+      return this.$withBase(this.$page.frontmatter.backdrop) || false;
+    }
+  }
 };
 </script>
+
+<style scoped>
+.backdrop {
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+
