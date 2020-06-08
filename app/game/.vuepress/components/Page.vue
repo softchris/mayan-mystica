2be76e8ce029f22aebcd90723413a30b33cb24b6
Jen Looper <jen.looper@gmail.com>
2020-06-07 21:22:04 -0400

    build step path tweak for api

--- app/game/.vuepress/components/Page.vue
+++ app/game/.vuepress/components/Page.vue
@@ -1,79 +1,73 @@
 <template>
-  <div class="prompt">
-    <span class="font-bold" v-if="show">{{ instructions }}</span>
-    <div class="page-nav" v-if="show">
-      <router-link :to="link || './'">{{ action }}</router-link>
-    </div>
-  </div>
+	<div class="prompt">
+		<span class="font-bold" v-if="show">{{ instructions }}</span>
+		<div class="page-nav" v-if="show">
+			<router-link :to="link || './'">{{ action }}</router-link>
+		</div>
+	</div>
 </template>
 <script>
-import {
-  hasItem,
-  getUID,
-  setUID,
-  hasUID,
-  setSessionTicket
-} from "@theme/utils/helpers";
-import axios from "axios";
-import { EventBus } from "@theme/utils/event-bus";
-import { i18n } from "@theme/utils/i18n";
+import { hasItem, getUID, setUID, hasUID, setSessionTicket } from '@theme/utils/helpers';
+import axios from 'axios';
+import { EventBus } from '@theme/utils/event-bus';
+import { i18n } from '@theme/utils/i18n';
 
 export default {
-  props: ["url", "action", "condition", "instructions"],
+	props: ['url', 'action', 'condition', 'instructions'],
 
-  //i18n: {},
-  data() {
-    return {
-      show: false,
-      link: this.url
-    };
-  },
-  methods: {
-    getInventory(id) {
-      if (this.condition == id || this.condition == "none") {
-        this.show = true;
-      } else {
-        this.show = false;
-      }
-    },
-    goToRoom(url) {
-      this.$router.replace({ path: url });
-    }
-  },
-  created() {
-    EventBus.$on("item_added", id => {
-      this.getInventory(id);
-    });
+	//i18n: {},
+	data() {
+		return {
+			show: false,
+			link: this.url,
+		};
+	},
+	methods: {
+		getInventory(id) {
+			if (this.condition == id || this.condition == 'none') {
+				this.show = true;
+			} else {
+				this.show = false;
+			}
+		},
+		goToRoom(url) {
+			this.$router.replace({ path: url });
+		},
+	},
+	created() {
+		EventBus.$on('item_added', (id) => {
+			this.getInventory(id);
+		});
 
-    EventBus.$on("showResult", id => {
-      this.getInventory(id);
-    });
+		EventBus.$on('showResult', (id) => {
+			this.getInventory(id);
+		});
 
-    EventBus.$on("lang_changed", lang => (this.$i18n.locale = lang));
+		EventBus.$on('lang_changed', (lang) => (this.$i18n.locale = lang));
 
-    //initially, set home page to show followup
-    this.getInventory(1);
-    if (!hasUID()) {
-      setUID();
-    }
-    axios
-      .post("/AzureMayaMystery/api/loginAnon", {
-        id: getUID(),
-        createAccount: hasUID()
-      })
-      .then(response => {
-        console.log(response);
-        if (response.data.errorMessage == null) {
-          setSessionTicket(response.data.SessionTicket);
-        }
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  },
-  beforeDestroy() {
-    EventBus.$off("lang_changed");
-  }
+		//initially, set home page to show followup
+		this.getInventory(1);
+		if (!hasUID()) {
+			setUID();
+		}
+		axios
+			.post('/api/loginAnon', {
+				id: getUID(),
+				createAccount: hasUID(),
+			})
+			.then((response) => {
+				console.log(response);
+				if (response.data.errorMessage == null) {
+					setSessionTicket(response.data.SessionTicket);
+				}
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	},
+	beforeDestroy() {
+		EventBus.$off('lang_changed');
+	},
 };
 </script>
 <style lang="stylus">
