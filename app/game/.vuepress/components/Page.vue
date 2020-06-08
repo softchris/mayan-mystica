b3c2cf12d09c9914abadb4f7baf3124a589de710
Jen Looper <jen.looper@gmail.com>
2020-06-08 06:59:48 -0400

    full path for api calls?

--- app/game/.vuepress/components/Page.vue
+++ app/game/.vuepress/components/Page.vue
@@ -51,7 +51,7 @@ export default {
 			setUID();
 		}
 		axios
-			.post('/api/loginAnon', {
+			.post('https://white-field-0a3ef4c1e.azurestaticapps.net/api/loginAnon', {
 				id: getUID(),
 				createAccount: hasUID(),
 			})
