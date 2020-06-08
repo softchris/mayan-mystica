ead600e9b252a27662ec87d2e23bfb7f5fc324f8
Jen Looper <jen.looper@gmail.com>
2020-06-08 10:44:10 -0400

    attempting to reimplement internal API calls

--- app/game/.vuepress/components/Page.vue
+++ app/game/.vuepress/components/Page.vue
@@ -57,7 +57,7 @@ export default {
       setUID();
     }
     axios
-      .post("https://maya-mystery-api.azurewebsites.net/api/loginAnon", {
+      .post("https://white-field-0a3ef4c1e.azurestaticapps.net/api/loginAnon", {
         id: getUID(),
         createAccount: hasUID()
       })
