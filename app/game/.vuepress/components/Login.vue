921a4eca0596ab8e713564c26538c9b8a4c25d80
Jen Looper <jen.looper@gmail.com>
2020-06-19 10:26:34 -0400

    accessibility bugfix - add asterisk for required fields

--- app/game/.vuepress/components/Login.vue
+++ app/game/.vuepress/components/Login.vue
@@ -5,7 +5,10 @@
       <p class="text-red-500 text-xs italic" aria-live="polite">{{ message }}</p>
 
       <div class="mb-4">
-        <label class="mb-2" for="email">Email</label>
+        <label class="mb-2" for="email">
+          Email
+          <span class="text-red-500">*</span>
+        </label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="email"
@@ -17,7 +20,10 @@
         />
       </div>
       <div class="mb-6">
-        <label class="mb-2" for="password">Password</label>
+        <label class="mb-2" for="password">
+          Password
+          <span class="text-red-500">*</span>
+        </label>
         <input
           class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           id="password"
