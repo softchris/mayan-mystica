cf3b383900dfe46bc0bf576f486742ff975ff9fb
Jen Looper <jen.looper@gmail.com>
2020-08-31 16:34:07 -0400

    accessibility 22108 - order of floated selects

--- app/game/.vuepress/theme/layouts/BasicLayout.vue
+++ app/game/.vuepress/theme/layouts/BasicLayout.vue
@@ -1,58 +1,67 @@
 <template>
   <div class="markdown-body">
-    <div class="flex flex-wrap float-right m-2 mr-5">
-      <label
-        class="mr-3 float-left block tracking-wide text-white text-xs font-bold mb-2"
-        for="grid-state"
-      >{{ $t('level') }}</label>
-      <div class="relative">
-        <select
-          role="listbox"
-          @change="goToLevel($event)"
-          class="block appearance-none w-full border border-gray-200 py-3 px-4 pr-8 rounded leading-tight"
-          id="level"
-        >
-          <option role="link" value="1">1</option>
-          <option role="link" value="2">2</option>
-          <option role="link" value="3">3</option>
-        </select>
-        <div
-          class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-        >
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-          </svg>
+    <div class="float-right mr-3">
+      <div class="flex flex-wrap m-2 mr-5 float-left">
+        <label
+          class="mr-3 float-left block tracking-wide text-white text-xs font-bold mb-2"
+          for="grid-state"
+        >{{ $t('level') }}</label>
+        <div class="relative">
+          <select
+            role="listbox"
+            @change="goToLevel($event)"
+            class="block appearance-none w-full border border-gray-200 py-3 px-4 pr-8 rounded leading-tight"
+            id="level"
+          >
+            <option role="link" value="1">1</option>
+            <option role="link" value="2">2</option>
+            <option role="link" value="3">3</option>
+          </select>
+          <div
+            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+          >
+            <svg
+              class="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-wrap m-2">
+        <label
+          class="mr-3 float-left block tracking-wide text-white text-xs font-bold mb-2"
+          for="grid-state"
+        >{{ $t('language') }}</label>
+        <div class="relative">
+          <select
+            role="listbox"
+            @change="setLanguage($event)"
+            class="block appearance-none w-full border border-gray-200 py-3 px-4 pr-8 rounded leading-tight"
+            id="level"
+          >
+            <option role="link" value="en">English</option>
+            <option role="link" value="es">Español</option>
+            <option role="link" value="fr">Français</option>
+            <option role="link" value="pt">Português</option>
+          </select>
+          <div
+            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+          >
+            <svg
+              class="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
-
-    <div class="flex flex-wrap float-right m-2">
-      <label
-        class="mr-3 float-left block tracking-wide text-white text-xs font-bold mb-2"
-        for="grid-state"
-      >{{ $t('language') }}</label>
-      <div class="relative">
-        <select
-          role="listbox"
-          @change="setLanguage($event)"
-          class="block appearance-none w-full border border-gray-200 py-3 px-4 pr-8 rounded leading-tight"
-          id="level"
-        >
-          <option role="link" value="en">English</option>
-          <option role="link" value="es">Español</option>
-          <option role="link" value="fr">Français</option>
-          <option role="link" value="pt">Português</option>
-        </select>
-        <div
-          class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-        >
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-          </svg>
-        </div>
-      </div>
-    </div>
-
     <p class="text-3xl pb-5 pt-5 ml-5 text-sans">{{ $page.frontmatter.title }}</p>
     <div class="p-5 bg-white m-5 mt-6 rounded">
       <Content />
